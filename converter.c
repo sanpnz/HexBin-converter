1f3acf55f9627644ce0f2154de9eedeb97f22d0b
Alex <pnz.alexandr@yandex.ru>
2026-01-31 17:49:27 +0300

    Добавлена обработка агрумета -a. Добавлена проверка имени входного .hex файла. Добавлена функция конвертации HEX файла.

--- converter.c
+++ converter.c
@@ -1,6 +1,84 @@
 #include <stdio.h>
+#include <string.h>
 
-void main()
+// Функция конвертации HEX файла в бинарный
+// [in] pcInFileName    имя входнохо файла
+// [in] pcOutFileName   имя выходного файла
+// result               результат выполнения
+int ConvertHexToBin(const char *pcInFileName, const char *pcOutFileName) 
 {
-    printf("\nTest Project\n");
+    // Открываем на чтение файл
+    FILE *pInputFile = fopen(pcInFileName, "r");
+    if(!pInputFile)
+    {
+        printf("\nНе удалось открыть файл для чтения\n");
+        return 1;
+    }
+
+    // Открываем файл на запись
+    FILE *pOutputFile = fopen(pcOutFileName, "wb");
+    if(!pOutputFile)
+    {
+        printf("\nНе удалось открыть файл для записи\n");
+        fclose(pInputFile);
+        return 1;
+    }
+
+    fclose(pInputFile);
+    fclose(pOutputFile);
+
+    return 0;
+}
+
+int main(int argc, char *argv[]) 
+{
+    // Обработка вызова -h
+    if(argc == 2 && !strcmp(argv[1], "-h"))
+    {
+        printf("\nRequested help\n");
+        return 0;
+    }
+
+    // Проверка количества аргументов
+    if(argc != 3)
+    {
+        printf("\nОшибка. Неизвестный параметр.\nИнструкция по использованию -h\n");
+        return 1;
+
+    }
+
+    int nResult = 0;
+    // Получение имени файла
+    char *pcInFileName = argv[2];
+
+    // Проверка, что агрумент -a
+    if(!strcmp(argv[1], "-a"))
+    {
+        // Проверка, что имя файла не менее 5 символов
+        size_t nFileNameLen = strlen(pcInFileName);
+        if (nFileNameLen < 5) 
+        {
+            printf("\nНеверное имя файла.\n");
+            return 1;
+        }   
+
+        // Проверка, что имя файла имеет расширение .hex
+        if (strcmp(pcInFileName + nFileNameLen - 4, ".hex")) 
+        {
+            printf("\nНеверное имя файла.\n");
+            return 1;
+        }
+
+        // Формирование имени выходного файла
+        char pcOutFileName[nFileNameLen + 5];
+        strcpy(pcOutFileName, pcInFileName);
+        strcat(pcOutFileName, ".bin");
+        
+
+       nResult = ConvertHexToBin(pcInFileName, pcOutFileName);
+    }
+
+    printf("\nEnd Project\n");
+   
+    return nResult;
 }
